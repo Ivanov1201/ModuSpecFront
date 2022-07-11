05950f0150503e097d94831a67bc1aa48749b351
Smarttabdev <87866630+Smarttabdev@users.noreply.github.com>
2022-07-10 22:08:22 -0400

    custom gauge component added

---
+++ src/app/shared/components/smartguage/smartguage.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, DoCheck } from '@angular/core';
+import { Params } from './params';
+
+@Component({
+  selector: 'app-smartguage',
+  templateUrl: './smartguage.component.html',
+  styleUrls: ['./smartguage.component.css']
+})
+export class SmartguageComponent implements DoCheck {
+  @Input() params!: Params;
+  @Input() pointer = '';
+  circle = 0;
+  width = '80px';
+  height = '80px';
+  mainColor = 'rgb(74, 129, 238)';
+  bgColor = 'rgba(255, 255, 255, 0.2)';
+  constructor() { }
+
+  ngDoCheck(): void {
+    let value = Math.floor(260 * (this.params.value / (this.params.max - this.params.min) - 0.5));
+    this.pointer = 'rotate(' + value + 'deg)';
+    this.circle = 126 - 87 * (this.params.value / (this.params.max - this.params.min));
+
+    if ( this.params.width ) this.width = this.params.width;
+    if ( this.params.height ) this.height = this.params.height;
+    if ( this.params.color ) this.mainColor = this.params.color;
+    if ( this.params.bgcolor ) this.bgColor = this.params.bgcolor;
+  }
+
+  
+
+}
